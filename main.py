158dbcd424b0af3cf779d019448cb9ec3a044e67
unknown <Administrator@LAB-INFO-05.lab.saomiguel.ifal>
2022-07-26 11:05:56 -0300

    Inclusão do arquivo deny-list.txt

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from pid_killer import PID_Killer
 if __name__ == '__main__':
     # Matar processo
     while True:
-        obj = PID_Killer('Store')
-        obj.killer()
+        with open('deny-list.txt', 'r') as file:
+           data = file.readline()
+           obj = PID_Killer(data)
+           obj.killer()
