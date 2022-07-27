8d6778859b15233ce3f33ef11ab27405d3f2275e
Bruno Marques <bruno.mb.91@gmail.com>
2022-07-27 11:45:42 -0300

    Alterações na função killer

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from pid_killer import PID_Killer
 if __name__ == '__main__':
     # Matar processo
     while True:
-        with open('deny-list.txt', 'r') as file:
-           data = file.readline()
-           obj = PID_Killer(data)
-           obj.killer()
+        processes_to_kill = []
+        
+        with open('deny-list.txt', 'r') as f:
+            while f.readline() != '':
+                processes_to_kill.append(f.readline())
+
+        for data in processes_to_kill:
+            obj = PID_Killer(data)
+            obj.killer()
+
--- pid_killer.py
+++ pid_killer.py
@@ -3,18 +3,26 @@ import os
 import signal
 
 class PID_Killer:
-    def __init__(self, processName):
-        self.__processName = processName
+    def __init__(self, fileOfProcesses):
+        # self.__processName = processName
+        self.__fileOfProcesses = fileOfProcesses
 
     def killer(self):
         f = wmi.WMI()
         pid_list = []
 
+        listOfNames = []
+        fp = open('deny-list.txt', 'r')
+        listOfNames = fp.read().split('\n')
+        
         # Buscar programas pelo nome
-        for i in f.Win32_Process():
-            if i.name.count(self.__processName) >= 1 or i.name.count(self.__processName.lower()) >= 1:
-                pid_list.append(i.ProcessId)
-                # print(i.ProcessId)
+        
+        for name in listOfNames:
+            for i in f.Win32_Process():
+                if i.name.count(name) >= 1 or i.name.count(name.lower()) >= 1:
+                    pid_list.append(i.ProcessId)
+                    # print(i.ProcessId)
+
         for pid in pid_list:
             try:
                 os.kill(pid, signal.SIGABRT)
@@ -24,3 +32,4 @@ class PID_Killer:
                 print ("Execução do programa interrompida")
 
         pid_list.clear()
+    
